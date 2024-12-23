41e7af21835b1c2dffc8a54a0b896a7a45221188
Kuhn, Matthew L <mlkuhn@tva.gov>
2024-12-23 12:55:50 -0500

    console.assert

--- tests/utils/utils.js
+++ tests/utils/utils.js
@@ -1,19 +1,13 @@
 function assertTrue(expected) {
-  if (expected !== true) {
-    throw new Error(`expected: ${expected} is not true`);
-  }
+  console.assert(expected === true, `expected: ${expected} is not true`)
 }
 
 function assertEquals(actual, expected) {
-  if (actual !== expected) {
-    throw new Error(`actual: ${actual} does not equal expected: ${expected}`);
-  }
+  console.assert(actual === expected, `actual: ${actual} does not equal expected: ${expected}`)
 }
 
 function assertDefined(actual) {
-  if (actual == undefined) {
-    throw new Error(`actual: ${actual} is undefined`);
-  }
+  console.assert(actual != undefined, `actual: ${actual} is undefined`)
 }
 
 function assertElementExists(selector) {
