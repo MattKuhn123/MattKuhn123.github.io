01076b3b29caa0635a10a2a073d383f6591e9b5b
Kuhn, Matthew L <mlkuhn@tva.gov>
2025-01-03 10:57:51 -0500

    iterating thru tests

--- tests/run-tests.ps1
+++ tests/run-tests.ps1
@@ -1,1 +1,7 @@
-Start-Process -FilePath "msedge" -ArgumentList "C:\Users\mlkuhn\source\rogue\MattKuhn123.github.io\tests\mk-directives.html --user-data-dir=C:\msedge-dev-data\ --disable-web-security"
+$testDirectory = "$PSScriptRoot\pages"
+$testFiles = Get-ChildItem -Path $testDirectory -File
+foreach ($testFile in $testFiles) {
+  $logFile = "$PSScriptRoot\output\result-$testFile.log"
+  $testFileName = $testFile.FullName
+  Start-Process -FilePath "msedge" -ArgumentList "$testFileName --user-data-dir=C:\msedge-dev-data\ --disable-web-security --enable-logging --i=1 --log-file=$logFile"
+}
